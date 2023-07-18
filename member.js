2ab2bf5ddb19236cd1bed5c830b850250d7a4e71
Ngozi Okwuosa <73789529+zeelyha@users.noreply.github.com>
2023-07-18 10:40:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var about = document.getElementById("about");
+  var home = document.getElementById("home"); 
+}
